b8cd96acdd64a3896ed9fc3005b5d0d72078a35d
gaashg <24541470+gaashg@users.noreply.github.com>
2018-10-31 11:06:12 +0200

    separate classes

--- src/main/scala/com/tikalk/kafkastreams/KafkaStreamsMain.scala
+++ src/main/scala/com/tikalk/kafkastreams/KafkaStreamsMain.scala
@@ -1,26 +1,7 @@
 package com.tikalk.kafkastreams
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.tikalk.kafkastreams.config.KafkaStreamsConfig
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
-
-
-@SpringBootApplication
-class KafkaStreamsConfig {
-  @Bean
-  def mappingJackson2HttpMessageConverter: MappingJackson2HttpMessageConverter = {
-    val mapping = new MappingJackson2HttpMessageConverter
-    val objectMapper = new ObjectMapper()
-    objectMapper.registerModule(DefaultScalaModule)
-    import com.fasterxml.jackson.databind.DeserializationFeature
-    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-    mapping.setObjectMapper(objectMapper)
-    mapping
-  }
-}
 
 
 object KafkaStreamsMain extends App {
---
+++ src/main/scala/com/tikalk/kafkastreams/config/KafkaStreamsConfig.scala
@@ -0,0 +1,22 @@
+package com.tikalk.kafkastreams.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+
+
+@SpringBootApplication
+class KafkaStreamsConfig {
+  @Bean
+  def mappingJackson2HttpMessageConverter: MappingJackson2HttpMessageConverter = {
+    val mapping = new MappingJackson2HttpMessageConverter
+    val objectMapper = new ObjectMapper()
+    objectMapper.registerModule(DefaultScalaModule)
+    import com.fasterxml.jackson.databind.DeserializationFeature
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    mapping.setObjectMapper(objectMapper)
+    mapping
+  }
+}
