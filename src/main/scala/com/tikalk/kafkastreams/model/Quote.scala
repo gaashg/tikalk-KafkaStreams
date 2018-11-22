2cf3f71693f3b49a93faecb64b9315311be0d4a7
Gaash <24541470+gaashg@users.noreply.github.com>
2018-11-22 15:29:43 +0200

    Update Quote.scala

--- src/main/scala/com/tikalk/kafkastreams/model/Quote.scala
+++ src/main/scala/com/tikalk/kafkastreams/model/Quote.scala
@@ -1,5 +1,5 @@
 package com.tikalk.kafkastreams.model
 
-class Quote(id: String, creationDate: Long, var line: String, var character: String, var season: String, var episode: String) extends BaseEvent (id, creationDate){
+class Quote(id: String, creationDate: Long, version: Int, var line: String, var character: String, var season: String, var episode: String) extends BaseEvent (id, creationDate, version){
   override def toString: String = s"Quote [line=$line, character=$character, season=$season, episode=$episode, " + super.toString + "]"
 }
