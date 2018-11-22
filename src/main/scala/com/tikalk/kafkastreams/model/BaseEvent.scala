b5474902ec66d8050e3f809af1eb642fca933b4b
Gaash <24541470+gaashg@users.noreply.github.com>
2018-11-22 15:28:51 +0200

    Update BaseEvent.scala

--- src/main/scala/com/tikalk/kafkastreams/model/BaseEvent.scala
+++ src/main/scala/com/tikalk/kafkastreams/model/BaseEvent.scala
@@ -1,5 +1,5 @@
 package com.tikalk.kafkastreams.model
 
-class BaseEvent (var id : String, var creationDate: Long) {
-  override def toString: String = s"BaseEvent [id=$id, creationDate=$creationDate]"
+class BaseEvent (var id : String, var creationDate: Long, version : Int) {
+  override def toString: String = s"BaseEvent [id=$id, creationDate=$creationDate, version=$version]"
 }
