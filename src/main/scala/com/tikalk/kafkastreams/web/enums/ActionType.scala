3522991ff61544c2da5222a7026e56611307df22
gaashg <24541470+gaashg@users.noreply.github.com>
2018-11-25 15:26:07 +0200

    resources

--- src/main/scala/com/tikalk/kafkastreams/web/enums/ActionType.scala
+++ src/main/scala/com/tikalk/kafkastreams/web/enums/ActionType.scala
@@ -2,5 +2,5 @@ package com.tikalk.kafkastreams.web.enums
 
 object ActionType extends Enumeration {
   type ActionType = Value
-  val ADD_NEW_QUOTE = Value
+  val ADD_NEW_QUOTE, GET_PLAYERS_STATISTICS = Value
 }
