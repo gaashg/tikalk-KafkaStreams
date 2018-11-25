ebbcfb88884cd69e18a6c85503eabfa77f0fb356
gaashg <24541470+gaashg@users.noreply.github.com>
2018-11-25 15:27:24 +0200

    resources

--- src/main/scala/com/tikalk/kafkastreams/web/controllers/StatisticsController.scala
+++ src/main/scala/com/tikalk/kafkastreams/web/controllers/StatisticsController.scala
@@ -1,8 +1,20 @@
 package com.tikalk.kafkastreams.web.controllers
 
-import org.springframework.web.bind.annotation.RestController
+import com.tikalk.kafkastreams.common.model.{ActionResult, PlayersStatistics}
+import com.tikalk.kafkastreams.web.enums.ActionType
+import com.tikalk.kafkastreams.web.services.StatisticsService
+import javax.annotation.Resource
+import org.springframework.web.bind.annotation.{GetMapping, PostMapping, ResponseBody, RestController}
 
 @RestController
 class StatisticsController {
+  @Resource
+  val statisticsService : StatisticsService = null
 
+  @GetMapping(value=Array("/players"))
+  @ResponseBody
+  def getPlayersStatistics () : ActionResult = {
+    val statistics = statisticsService.getPlayersStatistics()
+    ActionResult(true, ActionType.GET_PLAYERS_STATISTICS, null, statistics)
+  }
 }
