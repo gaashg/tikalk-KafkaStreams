602dd0d1f7b582d0056b5a53771e9a2441b86e6b
Yoav Nordmann <Yoav.Nordmann@cyren.com>
2018-11-25 13:53:54 +0200

    Send Game into Kafka

--- src/main/scala/com/tikalk/kafkastreams/common/model/Game.scala
+++ src/main/scala/com/tikalk/kafkastreams/common/model/Game.scala
@@ -3,8 +3,10 @@ package com.tikalk.kafkastreams.common.model
 import com.tikalk.kafkastreams.web.enums.GameType.GameType
 
 class Game(id: String, creationDate: Long, gameType: GameType, playerIds: Array[String]) extends BaseEntity (id, creationDate, 1){
+  override def toString: String = s"GameType: $gameType, playerIds: $playerIds" + super.toString
+
 }
 
 object Game{
-  val TOPIC_NAME = "Gamess"
+  val TOPIC_NAME = "Games"
 }
--- src/main/scala/com/tikalk/kafkastreams/web/enums/GameType.scala
+++ src/main/scala/com/tikalk/kafkastreams/web/enums/GameType.scala
@@ -3,4 +3,5 @@ package com.tikalk.kafkastreams.web.enums
 object GameType extends Enumeration {
   type GameType = Value
   val SINGLE, MULTI, TOURNAMENT = Value
+
 }
