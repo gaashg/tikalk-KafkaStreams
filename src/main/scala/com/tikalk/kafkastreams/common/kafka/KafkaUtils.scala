60592cc9977c1e462c940f173a20070b6bb761bb
gaashg <24541470+gaashg@users.noreply.github.com>
2018-12-06 16:22:08 +0200

    better initialization

--- src/main/scala/com/tikalk/kafkastreams/common/kafka/KafkaUtils.scala
+++ src/main/scala/com/tikalk/kafkastreams/common/kafka/KafkaUtils.scala
@@ -2,14 +2,16 @@ package com.tikalk.kafkastreams.common.kafka
 
 import java.util.Properties
 
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig}
+import org.apache.kafka.clients.producer.ProducerConfig
 
 import scala.util.Random
 
 object KafkaUtils {
-  def initProperties() = {
+  def initProperties(keySerializerClassName: String, valueSerializerClassName: String) = {
     val props = new Properties()
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializerClassName)
+    props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializerClassName)
 
     props
   }
