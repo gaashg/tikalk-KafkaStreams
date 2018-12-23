b15196e468551106a1ce1ca65408d21f4c88840c
gaashg <24541470+gaashg@users.noreply.github.com>
2018-12-23 22:33:55 +0200

    add player configuration

--- src/main/scala/com/tikalk/kafkastreams/streams/games/GamesStream.scala
+++ src/main/scala/com/tikalk/kafkastreams/streams/games/GamesStream.scala
@@ -1,21 +1,38 @@
 package com.tikalk.kafkastreams.streams.games
 
+import com.tikalk.kafkastreams.common.kafka.KafkaConstants
+import com.tikalk.kafkastreams.common.model.Player
+import com.tikalk.kafkastreams.common.serdes.PlayerSerde
+import com.tikalk.kafkastreams.streams.utils.StreamsUtils
 import javax.annotation.PostConstruct
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.scala.kstream.{Consumed, Grouped, Materialized}
+import org.apache.kafka.streams.scala.{ByteArrayKeyValueStore, Serdes, StreamsBuilder}
 import org.slf4j.{Logger, LoggerFactory}
 import org.springframework.stereotype.Component
 
 @Component
 class GamesStream {
-  private val _logger : Logger = LoggerFactory.getLogger(classOf[GamesStream])
+  private val _logger: Logger = LoggerFactory.getLogger(classOf[GamesStream])
 
   @PostConstruct
-  def run () = {
+  def run() = {
     fetchPlayers()
   }
 
-  private def fetchPlayers () = {
+  private def fetchPlayers() = {
     _logger.info("listening to Players topic")
 
+    val props = StreamsUtils.initProperties()
+    props.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
+
+    implicit val consumed = Consumed.`with`[String, Player](Serdes.String, new PlayerSerde)
+    implicit val grouped = Grouped.`with`[String, Player](Serdes.String, new PlayerSerde)
+    implicit val materialized = Materialized.`with`[String, Player, ByteArrayKeyValueStore](Serdes.String, new PlayerSerde)
+
+    val builder: StreamsBuilder = new StreamsBuilder
+    val playersKTable = builder.stream[String, Player](KafkaConstants.PLAYER_TOPIC_NAME).groupByKey
+      .aggregate()
   }
 
 }
